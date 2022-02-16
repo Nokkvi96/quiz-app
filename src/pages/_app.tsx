c836b8a66d616d992b2a7a5d45e8ac19aa47f727
Nökkvi <nokkvi96@gmail.com>
2022-02-16 14:34:58 +0000

    react-query bætt við

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,29 @@
 /* eslint-disable import/no-duplicates */
 import type { AppProps } from "next/app";
 import { DefaultSeo } from "@utils/DefaultSeo";
-import { BaseLayout } from "@components/templates/BaseLayout";
-
 import { ThemeProvider } from "styled-components";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+import { BaseLayout } from "@components/templates/BaseLayout";
 import { theme } from "@theme/theme";
 import { GlobalStyle } from "@theme/GlobalStyle";
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 function MyApp({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient();
+
   return (
     <>
       <DefaultSeo />
       <ThemeProvider theme={theme}>
-        <>
-          <GlobalStyle />
-          <BaseLayout>
-            <Component {...pageProps} />
-          </BaseLayout>
-        </>
+        <QueryClientProvider client={queryClient}>
+          <>
+            <GlobalStyle />
+            <BaseLayout>
+              <Component {...pageProps} />
+            </BaseLayout>
+          </>
+        </QueryClientProvider>
       </ThemeProvider>
     </>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,30 @@
 import type { NextPage } from "next";
+import { useQuery } from "react-query";
 
-import { Grid } from "@components/system";
+import { Box, Grid, Text } from "@components/system";
+
+/**
+ *
+ * @returns
+ */
+const fetchQuiz = async () => {
+  const response = await fetch("https://quizapi.io/api/v1/questions", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "X-Api-Key": process.env.API_KEY,
+    },
+  });
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+  return response.json();
+};
 
 const Home: NextPage = () => {
+  const { isLoading, error, data } = useQuery("", fetchQuiz);
+  console.log(isLoading, error, data);
+
   return (
     <Grid
       gridGap={[4, null, 6]}
@@ -11,7 +33,13 @@ const Home: NextPage = () => {
         "repeat(2, 1fr)",
         "repeat(3, 1fr)",
       ]}
-    ></Grid>
+    >
+      {data?.map((d: any) => (
+        <Box key={d.id}>
+          <Text>{d.question}</Text>
+        </Box>
+      ))}
+    </Grid>
   );
 };
 
