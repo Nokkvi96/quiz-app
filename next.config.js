4f458ff354e6985ed90ca448630bc7558052577b
Nökkvi <nokkvi96@gmail.com>
2022-03-13 11:58:24 +0000

    updated to next 12.1

--- next.config.js
+++ next.config.js
@@ -3,8 +3,9 @@ const Dotenv = require("dotenv-webpack");
 /** @type {import('next').NextConfig} */
 module.exports = {
   swcMinify: true,
-  styledComponents: true,
-
+  compiler: {
+    styledComponents: true,
+  },
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Add the new plugin to the existing webpack plugins
     config.plugins.push(new Dotenv({ silent: true }));
